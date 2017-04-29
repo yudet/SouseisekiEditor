831103d7e3f3556b934e913ca59dd2cd69d0a750
yudet <yudetamago.wo@gmail.com>
2017-04-30 01:44:31 +0900

    build.js

--- build.js
+++ build.js
@@ -1,17 +1,18 @@
 var packager = require('electron-packager');  
 var config = require('./package.json');
+var platform = process.env.PLATFORM;
 
 packager({  
 	dir: './',
 	out: './release',
 	name: 'Souseiseki Editor',
-	platform: 'linux',
+	platform: platform,
 	arch: 'x64',
 
 	overwrite: true,
 	asar: true,
 	prune: true,
-	ignore: ['node_modules/(electron-packager|electron-prebuilt|\.bin)|release\.js','release/*','src/app/*'],
+	ignore: ['node_modules/(electron-packager|electron-prebuilt|\.bin)|release\.js','release/*','src/*'],
 }, function done (err, appPath) {
 	if(err) {
 		throw new Error(err);
